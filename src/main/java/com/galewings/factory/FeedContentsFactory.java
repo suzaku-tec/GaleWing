916942380516b31dd800c78437f3ec4e575ed00e
ikarashi <jhypd380@yahoo.co.jp>
2021-09-05 12:48:23 +0900

    refact: delete example package

---
+++ src/main/java/com/galewings/factory/FeedContentsFactory.java
@@ -0,0 +1,15 @@
+package com.galewings.factory;
+
+import com.galewings.entity.FeedContents;
+import com.rometools.rome.feed.synd.SyndEntry;
+
+public class FeedContentsFactory {
+
+  public static FeedContents create(SyndEntry entry) {
+    FeedContents contents = new FeedContents();
+    contents.uri = entry.getUri();
+    contents.type = entry.getContents().get(0).getType();
+    contents.value = entry.getContents().get(0).getValue();
+    return contents;
+  }
+}
