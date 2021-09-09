90c86180134d8e2dfe107b69909a2304a3ac72f4
ikarashi <jhypd380@yahoo.co.jp>
2021-09-09 20:56:14 +0900

    fix: library.db create batch

--- db_init.bat
+++ db_init.bat
@@ -1,7 +1,7 @@
 @echo off
 if not exist library.db (
   echo create DB
-  sqlite3.exe Sample.db < ddl.sql
+  sqlite3.exe library.db < ddl.sql
   echo create DB complete!!
 ) else (
   echo sqlite db exist
