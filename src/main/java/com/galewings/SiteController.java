3226e5f34fbbc5bfd2f0eefe0aa25b79782af224
ikarashi <jhypd380@yahoo.co.jp>
2021-09-26 01:16:20 +0900

    fix: add site management page
    - add site delete
    - add site list page

--- src/main/java/com/galewings/SiteController.java
+++ src/main/java/com/galewings/SiteController.java
@@ -2,19 +2,31 @@ package com.galewings;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galewings.dto.input.SiteDeleteDto;
 import com.galewings.entity.Site;
+import com.galewings.repository.SiteRepository;
 import com.miragesql.miragesql.ClasspathSqlResource;
 import com.miragesql.miragesql.SqlManager;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class SiteController {
 
   @Autowired
   SqlManager sqlManager;
+
+  @Autowired
+  SiteRepository siteRepository;
 
   /**
    * サイトリスト取得
@@ -26,10 +38,31 @@ public class SiteController {
   @ResponseBody
   @Transactional
   public String getSiteList() throws JsonProcessingException {
-    List<Site> resultList = sqlManager.getResultList(Site.class,
-        new ClasspathSqlResource("sql/site/select_all_site.sql"));
+    List<Site> resultList = siteList();
     ObjectMapper mapper = new ObjectMapper();
     return mapper.writeValueAsString(resultList);
   }
 
+  @RequestMapping(value = "/site/management", method = RequestMethod.GET)
+  @Transactional
+  public String index(Model model) {
+    List<Site> resultList = siteList();
+    model.addAttribute("sitelist", resultList);
+
+    return "/site/management";
+  }
+
+  @PostMapping(value = "/site/delete")
+  @ResponseBody
+  @Transactional
+  public String delete(@RequestBody SiteDeleteDto dto) {
+    boolean result = siteRepository.delete(dto.getUuid());
+    return Boolean.toString(result);
+  }
+
+  private List<Site> siteList() {
+    return sqlManager.getResultList(Site.class,
+        new ClasspathSqlResource("sql/site/select_all_site.sql"));
+
+  }
 }
--- src/main/javascript/src/galeWingApi.ts
+++ src/main/javascript/src/galeWingApi.ts
@@ -1,10 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export default class GaleWingApi {
   private readonly baseUrl: URL;
 
   private readonly apiUrls = {
     feedList: '/feedlist',
+    siteList: '/sitelist',
+    deleteSite: '/site/delete',
   };
 
   constructor() {
@@ -14,5 +16,27 @@ export default class GaleWingApi {
   getFeedList() {
     var ajaxUrl = this.baseUrl.origin + this.apiUrls.feedList + location.search;
     return axios.get(ajaxUrl);
+  }
+
+  getSiteList(url: string): Promise<AxiosResponse<any>> {
+    var baseUrl = new URL(url);
+
+    var ajaxUrl = baseUrl.origin + this.apiUrls.siteList;
+    return axios.get(ajaxUrl);
+  }
+
+  deleteSite(location: string, uuid: string) {
+    var baseUrl = new URL(location);
+    var ajaxUrl = baseUrl.origin + this.apiUrls.deleteSite;
+    return axios.post(ajaxUrl, { uuid: uuid });
+  }
+
+  private static singleton: GaleWingApi;
+  static getInstance() {
+    if (!this.singleton) {
+      this.singleton = new GaleWingApi();
+    }
+
+    return this.singleton;
   }
 }
