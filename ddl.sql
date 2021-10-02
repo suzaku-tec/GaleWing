c179054ca55616c7f2755d94cd33bcb810a85b7a
ikarashi <jhypd380@yahoo.co.jp>
2021-10-02 10:05:08 +0900

    fix: sqlmanager summary repository

--- ddl.sql
+++ ddl.sql
@@ -2,26 +2,6 @@ CREATE TABLE [feed_contents] ([uri] TEXT,
 [value] TEXT,
 [type] TEXT
 );
-CREATE TABLE [site] (
-[uuid] TEXT NOT NULL,
-[title] TEXT,
-[htmlUrl] TEXT,
-[xmlUrl] TEXT,
-PRIMARY KEY(uuid)
-);
-CREATE TABLE [settings_class] (
-[id] TEXT, --ID
-[name] TEXT, --項目名
-[parendId] TEXT, --親項目
-PRIMARY KEY(id)
-);
-CREATE TABLE [setting] (
-[classId] TEXT,
-[id] TEXT,
-[name] TEXT,
-[setting] TEXT,
-PRIMARY KEY(id)
-);
 CREATE TABLE [feed] (
 [title] TEXT,
 [uuid] TEXT,
@@ -32,4 +12,19 @@ CREATE TABLE [feed] (
 [publishedDate] TEXT,
 [readed] BOOLEAN NOT NULL DEFAULT '0',
 PRIMARY KEY(uuid,link)
+);
+CREATE TABLE [setting] (
+[id] TEXT,
+[setting] TEXT,
+[overview] TEXT,
+[description] TEXT,
+PRIMARY KEY(id)
+);
+CREATE TABLE [site] (
+[uuid] TEXT,
+[title] TEXT,
+[htmlUrl] TEXT,
+[xmlUrl] TEXT,
+[faviconBase64] TEXT,
+PRIMARY KEY(uuid)
 );
