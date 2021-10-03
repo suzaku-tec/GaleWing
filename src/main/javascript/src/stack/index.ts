2058cc6451e88dd2a1b87eef693a18b6ead6089a
ikarashi <jhypd380@yahoo.co.jp>
2021-10-04 00:11:14 +0900

    add: stack
    - stakking
    - view stacklist
    
    # 将来的に対応予定
    - 積読したら既読状態にする

---
+++ src/main/javascript/src/stack/index.ts
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'gridjs/dist/theme/mermaid.css';
+
+import hideModifier from '@popperjs/core/lib/modifiers/hide';
+import { Grid } from 'gridjs';
+
+import GaleWingApi from '../galeWingApi';
+
+window.onload = () => {
+  var api = GaleWingApi.getInstance();
+
+  api.getStackList(window.location.href).then((res) => {
+    console.log(res.data);
+    var grid = new Grid({
+      columns: [
+        { name: 'title' },
+        { name: 'uuid', hidden: true },
+        { name: 'link', hidden: true },
+        { name: 'uri', hidden: true },
+        { name: 'author', hidden: true },
+        { name: 'comments', hidden: true },
+        { name: 'publishedDate' },
+        { name: 'stackDate' },
+      ],
+      pagination: true,
+      sort: true,
+      search: true,
+      data: res.data,
+    }).render(document.getElementById('wrapper'));
+  });
+};
+
+export default { hideModifier };
