009899c66e637b09729a348d281ce65a4c568f18
ikarashi <jhypd380@yahoo.co.jp>
2023-03-19 22:37:36 +0900

    fix show feed add feed image fix test case add imageUrl column add FeedFactoryTest

--- src/test/java/com/galewings/factory/FeedFactoryTest.java
+++ src/test/java/com/galewings/factory/FeedFactoryTest.java
@@ -1,7 +1,12 @@
 package com.galewings.factory;
 
 import com.galewings.entity.Feed;
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndContentImpl;
 import com.rometools.rome.feed.synd.SyndEntryImpl;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +28,41 @@ class FeedFactoryTest {
     Assertions.assertEquals(expected.publishedDate, result.publishedDate);
     Assertions.assertEquals(expected.opened, result.opened);
     Assertions.assertEquals(expected.readed, result.readed);
+  }
+
+  @Test
+  void testCreate2() throws ParseException {
+    SyndEntryImpl syndEntry = new SyndEntryImpl();
+
+    SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    syndEntry.setPublishedDate(sdFormat.parse("2000-01-01 00:00:00"));
+
+    List<SyndContent> listContent = List.of(createSyndContentTestData("text/html", "<img src='a'>"),
+        createSyndContentTestData("html", "<img src='a'>"), createSyndContentTestData("", ""));
+    syndEntry.setContents(listContent);
+
+    Feed result = FeedFactory.create(syndEntry, "uuid");
+    Feed expected = new Feed();
+    expected.uuid = "uuid";
+    expected.author = "";
+    expected.publishedDate = "2000-01-01 12:00:00";
+
+    Assertions.assertEquals(expected.title, result.title);
+    Assertions.assertEquals(expected.uuid, result.uuid);
+    Assertions.assertEquals(expected.link, result.link);
+    Assertions.assertEquals(expected.uri, result.uri);
+    Assertions.assertEquals(expected.author, result.author);
+    Assertions.assertEquals(expected.comments, result.comments);
+    Assertions.assertEquals(expected.publishedDate, result.publishedDate);
+    Assertions.assertEquals(expected.opened, result.opened);
+    Assertions.assertEquals(expected.readed, result.readed);
+  }
+
+  private SyndContent createSyndContentTestData(String type, String value) {
+    SyndContent content = new SyndContentImpl();
+    content.setType(type);
+    content.setValue(value);
+    return content;
   }
 }
 
