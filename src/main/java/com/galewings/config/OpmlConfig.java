0edbbc4c18d5549df5ce988dec4973eb12796e80
suzaku <jhypd380@yahoo.co.jp>
2023-07-27 23:58:01 +0900

    fix export opml
    fix Thymeleaf engine

--- src/main/java/com/galewings/config/OpmlConfig.java
+++ src/main/java/com/galewings/config/OpmlConfig.java
@@ -2,7 +2,7 @@ package com.galewings.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
@@ -12,20 +12,20 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 @Configuration
 public class OpmlConfig {
 
-  /**
-   * OPMLテンプレートエンジン
-   *
-   * @return OPMLテンプレートエンジン
-   */
-  @Bean
-  public TemplateEngine opmlTemplateEngine() {
-    ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-    TemplateEngine engine = new TemplateEngine();
-    resolver.setTemplateMode(TemplateMode.TEXT);
-    resolver.setPrefix("templates/");
-    resolver.setSuffix(".opml");
-    engine.setTemplateResolver(resolver);
-    return engine;
-  }
+    /**
+     * OPMLテンプレートエンジン
+     *
+     * @return OPMLテンプレートエンジン
+     */
+    @Bean
+    public SpringTemplateEngine opmlTemplateEngine() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        resolver.setTemplateMode(TemplateMode.TEXT);
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".opml");
+        engine.setTemplateResolver(resolver);
+        return engine;
+    }
 
 }
