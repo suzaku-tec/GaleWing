5c0cb12168b377488c490b6057bdcd2b8dda6b0c
suzaku <jhypd380@yahoo.co.jp>
2023-11-26 00:44:52 +0900

    add card layout page

--- src/main/java/com/galewings/controller/GaleWingsController.java
+++ src/main/java/com/galewings/controller/GaleWingsController.java
@@ -2,6 +2,7 @@ package com.galewings.controller;
 
 import com.galewings.entity.SiteFeedCount;
 import com.galewings.repository.SiteRepository;
+import com.rometools.utils.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,10 +41,30 @@ public class GaleWingsController {
     }
 
     @GetMapping(value = "/card/")
-    public String cardLayout(Model model, @RequestParam(name = "uuid", required = false) String uuid) {
+    public String cardLayout(Model model, @RequestParam(name = "uuid", required = false) String uuid,
+                             @RequestParam(name = "index", required = false) String index) {
         List<SiteFeedCount> resultList = siteRepository.getSiteFeedCount();
         model.addAttribute("sitelist", resultList);
         model.addAttribute("identifier", uuid);
+
+        int pageIndex = Strings.isBlank(index) ? 1 : Integer.parseInt(index);
+        model.addAttribute("pageIndex", pageIndex);
+
+        int startIndex = 1;
+        int endIndex = resultList.size();
+        model.addAttribute("startIndex", startIndex);
+        model.addAttribute("endIndex", endIndex);
+
+        int previousIndex = pageIndex == 1 ? 1 : pageIndex - 1;
+        int nextIndex = pageIndex == endIndex ? endIndex : pageIndex + 1;
+        model.addAttribute("previousIndex", previousIndex);
+        model.addAttribute("nextIndex", nextIndex);
+
+        boolean startSpace = 1 < previousIndex;
+        boolean endSpace = nextIndex < endIndex;
+        model.addAttribute("startSpace", startSpace);
+        model.addAttribute("endSpace", endSpace);
+
         return "card";
     }
 
