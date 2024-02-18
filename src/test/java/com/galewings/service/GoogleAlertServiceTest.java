f2a97d41f03506b75fef39588c7f501a362d3fe8
suzaku <jhypd380@yahoo.co.jp>
2024-02-18 18:20:46 +0900

    fix update google alert
    fix Complexity
    fix testcase

--- src/test/java/com/galewings/service/GoogleAlertServiceTest.java
+++ src/test/java/com/galewings/service/GoogleAlertServiceTest.java
@@ -2,18 +2,24 @@ package com.galewings.service;
 
 import com.galewings.entity.Site;
 import com.galewings.repository.FeedRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
 import java.time.Month;
 
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class GoogleAlertServiceTest {
     @Mock
@@ -39,8 +45,56 @@ class GoogleAlertServiceTest {
         when(gwDateService.now()).thenReturn(LocalDate.of(2024, Month.FEBRUARY, 18));
         when(feedRepository.existFeed(anyString())).thenReturn(true);
 
-        googleAlertService.updateFeed(new Site());
+        Document doc = Jsoup.parse("<entry><id>test</id><title>test</title><link>test</link></entry>");
+        Connection con = JsoupTestMock.getConnectionMockInstance(doc);
+
+        try (MockedStatic<Jsoup> mocked = mockStatic(Jsoup.class)) {
+            mocked.when(() -> Jsoup.connect(anyString())).thenReturn(con);
+            googleAlertService.updateFeed(new Site());
+        }
     }
+
+    @Test
+    void testUpdateFeedRuntimeException() {
+        when(gwDateService.now()).thenReturn(LocalDate.of(2024, Month.FEBRUARY, 18));
+        when(feedRepository.existFeed(anyString())).thenThrow(new RuntimeException());
+
+        Site site = new Site();
+        site.title = "site:https";
+        site.xmlUrl = StringUtils.EMPTY;
+
+        Document doc = Jsoup.parse("<entry><id>test</id><title>test</title><link>test</link></entry>");
+        Connection con = JsoupTestMock.getConnectionMockInstance(doc);
+
+        try (MockedStatic<Jsoup> mocked = mockStatic(Jsoup.class)) {
+            mocked.when(() -> Jsoup.connect(anyString())).thenReturn(con);
+            googleAlertService.updateFeed(site);
+            fail();
+        } catch (RuntimeException e) {
+
+        }
+    }
+
+    @Test
+    void testUpdateFeedTrue() {
+        when(gwDateService.now()).thenReturn(LocalDate.of(2024, Month.FEBRUARY, 18));
+        when(feedRepository.existFeed(anyString())).thenReturn(false);
+        when(feedRepository.insertEntity(any())).thenReturn(0);
+
+        Site site = new Site();
+        site.title = "site:https";
+        site.xmlUrl = StringUtils.EMPTY;
+
+        Document doc = Jsoup.parse("<entry><id>test</id><title>test</title><link>test</link></entry>");
+        Connection con = JsoupTestMock.getConnectionMockInstance(doc);
+
+        try (MockedStatic<Jsoup> mocked = mockStatic(Jsoup.class)) {
+            mocked.when(() -> Jsoup.connect(anyString())).thenReturn(con);
+            googleAlertService.updateFeed(site);
+        }
+    }
+
+
 }
 
 //Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
