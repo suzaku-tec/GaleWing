7c544294d27d29b68313f4b3dfaaecb7c2af9727
suzaku <jhypd380@yahoo.co.jp>
2024-02-18 15:28:25 +0900

    fix update google alert
    fix Complexity
    fix testcase

--- src/test/java/com/galewings/task/AutoUpdateTaskTest.java
+++ src/test/java/com/galewings/task/AutoUpdateTaskTest.java
@@ -1,45 +1,52 @@
 package com.galewings.task;
 
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
-
 import com.galewings.entity.Site;
 import com.galewings.repository.FeedRepository;
 import com.galewings.repository.SiteRepository;
+import com.galewings.service.GoogleAlertService;
 import com.galewings.service.GwDateService;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
 class AutoUpdateTaskTest {
 
-  @Mock
-  SiteRepository siteRepository;
+    @Mock
+    SiteRepository siteRepository;
 
-  @Mock
-  FeedRepository feedRepository;
+    @Mock
+    FeedRepository feedRepository;
 
-  @Mock
-  GwDateService gwDateService;
+    @Mock
+    GwDateService gwDateService;
 
-  @InjectMocks
-  AutoUpdateTask autoUpdateTask;
+    @Mock
+    private GoogleAlertService googleAlertService;
 
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.initMocks(this);
-  }
+    @InjectMocks
+    AutoUpdateTask autoUpdateTask;
 
-  @Test
-  void testAllUpdate() {
-    when(siteRepository.getAllSite()).thenReturn(List.of(new Site()));
-    when(feedRepository.existFeed(anyString())).thenReturn(true);
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-    autoUpdateTask.allUpdate();
-  }
+    @Test
+    void testAllUpdate() {
+        when(siteRepository.getAllSite()).thenReturn(List.of(new Site()));
+        when(googleAlertService.isGoogleAlert(any())).thenReturn(false);
+        when(feedRepository.existFeed(anyString())).thenReturn(true);
+
+        autoUpdateTask.allUpdate();
+    }
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
