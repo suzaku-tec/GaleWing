619eb026e45c6804a891baaa972ed0a495b16b15
suzaku <jhypd380@yahoo.co.jp>
2024-02-23 15:45:00 +0900

    fix google ai
    add gemini api req/res
    add test case

--- src/test/java/com/galewings/service/GeminiServiceTest.java
+++ src/test/java/com/galewings/service/GeminiServiceTest.java
@@ -1,11 +1,13 @@
 package com.galewings.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.galewings.dto.ai.googleai.response.GeminiResponseDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
@@ -16,7 +18,14 @@ import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 class GeminiServiceTest {
-    GeminiService geminiService = new GeminiService();
+
+    @InjectMocks
+    private GeminiService geminiService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void testTellMe() throws IOException, InterruptedException {
@@ -35,35 +44,64 @@ class GeminiServiceTest {
     }
 
     @Test
-    void testTellMe_JsonProcessingException() {
+    void testTellMe_JsonProcessingException() throws IOException, InterruptedException {
+        HttpClient mockClient = Mockito.mock(HttpClient.class);
+        HttpResponse responseMock = Mockito.mock(HttpResponse.class);
+
+        when(mockClient.send(any(), any())).thenReturn(responseMock);
+        when(responseMock.body()).thenReturn("{ test");
+
         try (MockedStatic<HttpClient> mocked = mockStatic(HttpClient.class)) {
-            mocked.when(HttpClient::newHttpClient).thenThrow(Mockito.mock(JsonProcessingException.class));
+            mocked.when(HttpClient::newHttpClient).thenReturn(mockClient);
+
             GeminiResponseDto result = geminiService.tellMe("text");
             Assertions.fail();
         } catch (RuntimeException e) {
             // 正常
+        } catch (Exception e) {
+            Assertions.fail();
         }
     }
 
     @Test
-    void testTellMe_IOException() {
+    void testTellMe_IOException() throws IOException, InterruptedException {
+        HttpClient mockClient = Mockito.mock(HttpClient.class);
+        HttpResponse responseMock = Mockito.mock(HttpResponse.class);
+
+        // 例外設定
+        when(mockClient.send(any(), any())).thenThrow(new IOException());
+        when(responseMock.body()).thenThrow(new RuntimeException()); // これは実行されないはず
+
         try (MockedStatic<HttpClient> mocked = mockStatic(HttpClient.class)) {
-            mocked.when(HttpClient::newHttpClient).thenThrow(Mockito.mock(IOException.class));
+            mocked.when(HttpClient::newHttpClient).thenReturn(mockClient);
+
             GeminiResponseDto result = geminiService.tellMe("text");
             Assertions.fail();
         } catch (RuntimeException e) {
             // 正常
+        } catch (Exception e) {
+            Assertions.fail();
         }
     }
 
     @Test
-    void testTellMe_InterruptedException() {
+    void testTellMe_InterruptedException() throws IOException, InterruptedException {
+        HttpClient mockClient = Mockito.mock(HttpClient.class);
+        HttpResponse responseMock = Mockito.mock(HttpResponse.class);
+
+        // 例外設定
+        when(mockClient.send(any(), any())).thenThrow(new InterruptedException());
+        when(responseMock.body()).thenThrow(new RuntimeException()); // これは実行されないはず
+
         try (MockedStatic<HttpClient> mocked = mockStatic(HttpClient.class)) {
-            mocked.when(HttpClient::newHttpClient).thenThrow(Mockito.mock(InterruptedException.class));
+            mocked.when(HttpClient::newHttpClient).thenReturn(mockClient);
+
             GeminiResponseDto result = geminiService.tellMe("text");
             Assertions.fail();
         } catch (RuntimeException e) {
             // 正常
+        } catch (Exception e) {
+            Assertions.fail();
         }
     }
 
