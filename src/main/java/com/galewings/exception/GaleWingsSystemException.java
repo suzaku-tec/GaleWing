f2aa39187ea23de34ec9ef38894d52fb2ba39b28
suzaku <jhypd380@yahoo.co.jp>
2024-03-08 13:28:15 +0900

    add summary page
    fix testcase
    fix Coverage
    fix exception

--- src/main/java/com/galewings/exception/GaleWingsSystemException.java
+++ src/main/java/com/galewings/exception/GaleWingsSystemException.java
@@ -2,6 +2,9 @@ package com.galewings.exception;
 
 public class GaleWingsSystemException extends RuntimeException {
 
+    public GaleWingsSystemException() {
+    }
+
     public GaleWingsSystemException(InterruptedException e) {
         super(e);
     }
