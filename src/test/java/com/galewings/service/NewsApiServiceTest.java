90378d41615957fadf6f31793b5297a6a62c3abc
suzaku <jhypd380@yahoo.co.jp>
2024-10-14 17:48:57 +0900

    add knip
    fix error message in NewsApiService
    delete non use library
    add new error test case

--- src/test/java/com/galewings/service/NewsApiServiceTest.java
+++ src/test/java/com/galewings/service/NewsApiServiceTest.java
@@ -2,6 +2,7 @@ package com.galewings.service;
 
 import com.galewings.dto.newsapi.request.OptionalRequestParam;
 import com.galewings.dto.newsapi.response.NewsApiResponseDto;
+import com.galewings.exception.GaleWingsSystemException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestClient;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import static org.junit.Assert.fail;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -73,6 +75,30 @@ class NewsApiServiceTest {
 
     }
 
+    @Test
+    void testNoApikey() throws URISyntaxException, IOException, IllegalAccessException {
+        String apiKey = null;
+
+        var restClientBuilder = RestClient.builder();
+        MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restClientBuilder).build();
+        mockServer.expect(requestTo("/top-headlines?country=jp&apiKey=" + apiKey + "&category=test"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess().body("{\"status\":\"ok\",\"totalResults\":4145,\"articles\":[{\"source\":{\"id\":null,\"name\":\"ETFDailyNews\"},\"author\":\"MarketBeatNews\",\"title\":\"test\",\"description\":\"test description\",\"url\":\"https://localhost\",\"urlToImage\": null,\"publishedAt\":\"2024-09-17T08:44:33Z\",\"content\":\"test\"}]}"));
+
+        var restClient = restClientBuilder.build();
+        newsApiService = new NewsApiService(restClient);
+        ReflectionTestUtils.setField(newsApiService, "apiKey", apiKey);
+        OptionalRequestParam param = new OptionalRequestParam();
+        param.country = "jp";
+        param.category = "test";
+
+        try {
+            NewsApiResponseDto result = newsApiService.topHeadlines(param);
+            fail("Should throw GaleWingsSystemException");
+        } catch (GaleWingsSystemException e) {
+            Assertions.assertEquals("newsapi.api-key not found. add .env file", e.getMessage());
+        }
+    }
 }
 
 //Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
