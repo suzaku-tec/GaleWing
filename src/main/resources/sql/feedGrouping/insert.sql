391935e9fe7e4516d06c1474b588df8b9aa655a8
suzaku <jhypd380@yahoo.co.jp>
2025-08-25 08:55:04 +0900

    fix common modal dialog

--- src/main/resources/sql/feedGrouping/insert.sql
+++ src/main/resources/sql/feedGrouping/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO feed_relation
-(feedUuid1, feedUuid2, calcDate)
-VALUES(/*feedUuid1*/, /*feedUuid2*/, strftime('%Y%m%d', 'now'));
+(feedUuid1, feedUuid2, calcDate, score)
+VALUES(/*feedUuid1*/'uuid1', /*feedUuid2*/'uuid2', strftime('%Y%m%d', 'now'), /*score*/'0.0');
