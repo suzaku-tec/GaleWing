7f2c4782684315572ec826502f56c7896b955fa0
suzaku <jhypd380@yahoo.co.jp>
2025-11-30 12:27:26 +0900

    fix ollama
    - add ollama connect
    - add ollama config
    - add test case

---
+++ src/test/java/com/galewings/service/OllamaServiceTest.java
@@ -0,0 +1,34 @@
+package com.galewings.service;
+
+import com.galewings.util.OllamaClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+class OllamaServiceTest {
+    @Mock
+    OllamaClient ollamaClient;
+    @InjectMocks
+    OllamaService ollamaService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testTellMe() {
+        when(ollamaClient.generate(anyString(), anyString())).thenReturn("generateResponse");
+
+        String result = ollamaService.tellMe("text");
+        Assertions.assertEquals("generateResponse", result);
+    }
+}
+
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
