636f6f410ed6c8ae75c70c221e77c7a6aa6910ec
suzaku <jhypd380@yahoo.co.jp>
2025-11-30 12:41:36 +0900

    fix ollama
    - add ollama connect
    - add ollama config
    - add test case

--- src/main/java/com/galewings/service/OllamaService.java
+++ src/main/java/com/galewings/service/OllamaService.java
@@ -3,7 +3,9 @@ package com.galewings.service;
 import com.galewings.util.OllamaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OllamaService implements TellMeAi {
 
     private final OllamaClient ollamaClient;
