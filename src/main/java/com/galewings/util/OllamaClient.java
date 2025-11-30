0e5ac22887f4dcf705ac870e7ccffc564aab083b
suzaku <jhypd380@yahoo.co.jp>
2025-11-30 13:44:33 +0900

    fix ollama
    - add ollama connect
    - add ollama config
    - add test case

--- src/main/java/com/galewings/util/OllamaClient.java
+++ src/main/java/com/galewings/util/OllamaClient.java
@@ -2,6 +2,7 @@ package com.galewings.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -14,6 +15,7 @@ public class OllamaClient {
 
     private final WebClient webClient;
 
+    @Autowired
     public OllamaClient(WebClient ollamaWebClient) {
         this.webClient = ollamaWebClient;
     }
