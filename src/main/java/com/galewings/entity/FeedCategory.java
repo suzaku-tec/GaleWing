a2d7b6b518ab446ea408a7189ad72c9b62bee6cd
suzaku <jhypd380@yahoo.co.jp>
2025-12-04 09:21:02 +0900

    fix tag_links
    - Calculate relationships from tag information

---
+++ src/main/java/com/galewings/entity/FeedCategory.java
@@ -0,0 +1,10 @@
+package com.galewings.entity;
+
+public class FeedCategory {
+
+    public String link;
+    public String category;
+    public String relevance;
+    public String tag;
+    public String title;
+}
